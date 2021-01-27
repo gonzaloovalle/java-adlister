9e663fb868c1b4e7b218fca4e10e866cd00cdd36
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-01-27 11:42:14 -0600

    jdbc exercise

--- src/main/java/com/adlister/dao/DaoFactory.java
+++ src/main/java/com/adlister/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package com.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
